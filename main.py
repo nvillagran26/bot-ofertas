1b31c139721ba54ae10f76ccdb0cf42543665926
nvillagran26 <nvillagran205@gmail.com>
2026-03-15 11:17:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CANAL_ID = -1003797588613
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     teclado = [
@@ -34,6 +35,22 @@ https://simple.ripley.cl/
 https://www.solotodo.cl/
 """
         await update.message.reply_text(mensaje)
+        
+async def publicar(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    mensaje = """
+🔥 OFERTA
+
+🛍️ Falabella
+https://www.falabella.com/falabella-cl
+
+🛍️ Ripley
+https://simple.ripley.cl/
+
+💻 SoloTodo
+https://www.solotodo.cl/
+"""
+    await context.bot.send_message(chat_id=CANAL_ID, text=mensaje)
+    await update.message.reply_text("Oferta publicada en el canal ✅")
 
     elif texto == "🛍️ Tiendas":
         mensaje = """
@@ -59,6 +76,7 @@ https://www.solotodo.cl
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("publicar", publicar))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, manejar_mensaje))
 
 print("Bot funcionando...")
