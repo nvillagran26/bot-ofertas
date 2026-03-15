eb86d6cc71832a863173930cd2530e2ac20fb7eb
nvillagran26 <nvillagran205@gmail.com>
2026-03-15 11:01:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,65 @@
 import os
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
-usuarios = set()
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_id = update.message.chat_id
-    usuarios.add(user_id)
-    await update.message.reply_text("Hola! Soy tu bot de ofertas.")
+    teclado = [
+        ["🔥 Ver ofertas"],
+        ["🛍️ Tiendas", "💻 Tecnología"]
+    ]
 
-async def ofertas(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    mensaje = """
+    reply_markup = ReplyKeyboardMarkup(teclado, resize_keyboard=True)
+
+    await update.message.reply_text(
+        "Bienvenido al Bot de Ofertas 👋\nSelecciona una opción:",
+        reply_markup=reply_markup
+    )
+
+async def manejar_mensaje(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    texto = update.message.text
+
+    if texto == "🔥 Ver ofertas":
+        mensaje = """
 🔥 OFERTAS DEL DIA 🔥
 
-🛍️ Falabella
+🛍️ Falabella  
 https://www.falabella.com/falabella-cl
 
-🛍️ Ripley
+🛍️ Ripley  
 https://simple.ripley.cl/
 
-💻 SoloTodo
+💻 SoloTodo  
 https://www.solotodo.cl/
 """
-    await update.message.reply_text(mensaje)
+        await update.message.reply_text(mensaje)
+
+    elif texto == "🛍️ Tiendas":
+        mensaje = """
+🛍️ TIENDAS
+
+Falabella  
+https://www.falabella.com
+
+Ripley  
+https://simple.ripley.cl
+"""
+        await update.message.reply_text(mensaje)
+
+    elif texto == "💻 Tecnología":
+        mensaje = """
+💻 TECNOLOGÍA
+
+SoloTodo  
+https://www.solotodo.cl
+"""
+        await update.message.reply_text(mensaje)
 
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("ofertas", ofertas))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, manejar_mensaje))
 
 print("Bot funcionando...")
 
